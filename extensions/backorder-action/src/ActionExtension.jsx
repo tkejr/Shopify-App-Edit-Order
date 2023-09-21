d679f5fefe32c997aa54e5c90fcdbaf289679d84
tanmay <52064142+tanmay-kali@users.noreply.github.com>
2023-09-21 16:50:10 -0500

    clu version update

--- extensions/backorder-action/src/ActionExtension.jsx
+++ extensions/backorder-action/src/ActionExtension.jsx
@@ -1,4 +1,4 @@
-import {useEffect, useState} from 'react';
+import { useEffect, useState } from "react";
 import {
   reactExtension,
   useApi,
@@ -6,43 +6,27 @@ import {
   BlockStack,
   Button,
   Text,
-} from '@shopify/ui-extensions-react/admin';
+} from "@shopify/ui-extensions-react/admin";
 
 // The target used here must match the target used in the extension's toml file (./shopify.extension.toml)
-const TARGET = 'admin.product-details.action.render';
+const TARGET = "admin.product-details.action.render";
 
 export default reactExtension(TARGET, () => <App />);
 
 function App() {
   // The useApi hook provides access to several useful APIs like i18n, close, and data.
-  const {extension: {target}, i18n, close, data} = useApi(TARGET);
+  const {
+    extension: { target },
+    i18n,
+    close,
+    data,
+  } = useApi(TARGET);
 
-  const [productTitle, setProductTitle] = useState('');
+  const [productTitle, setProductTitle] = useState("");
   // Use direct API calls to fetch data from Shopify.
   // See https://shopify.dev/docs/api/admin-graphql for more information about Shopify's GraphQL API
   useEffect(() => {
-    (async function getProductInfo() {
-      const getProductQuery = {
-        query: `query Product($id: ID!) {
-          product(id: $id) {
-            title
-          }
-        }`,
-        variables: {id: data.selected[0].id},
-      };
-
-      const res = await fetch("shopify:admin/api/graphql.json", {
-        method: "POST",
-        body: JSON.stringify(getProductQuery),
-      });
-
-      if (!res.ok) {
-        console.error('Network error');
-      }
-
-      const productData = await res.json();
-      setProductTitle(productData.data.product.title);
-    })();
+    setProductTitle("Testing");
   }, []);
   return (
     // The AdminAction component provides an API for setting the title and actions of the Action extension wrapper.
@@ -50,7 +34,7 @@ function App() {
       primaryAction={
         <Button
           onPress={() => {
-            console.log('saving');
+            console.log("saving");
             close();
           }}
         >
@@ -60,7 +44,7 @@ function App() {
       secondaryAction={
         <Button
           onPress={() => {
-            console.log('closing');
+            console.log("closing");
             close();
           }}
         >
@@ -70,7 +54,7 @@ function App() {
     >
       <BlockStack>
         {/* Set the translation values for each supported language in the locales directory */}
-        <Text fontWeight="bold">{i18n.translate('welcome', {target})}</Text>
+        <Text fontWeight="bold">{i18n.translate("welcome", { target })}</Text>
         <Text>Current product: {productTitle}</Text>
       </BlockStack>
     </AdminAction>
