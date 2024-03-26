a2cdb9e739a1c97f881466856d3a3c9e17e2cdac
AlbertoIreneoGaucin <albertogaucin.ag@gmail.com>
2024-03-26 13:06:35 -0500

    logging for quantity change

--- web/index.js
+++ web/index.js
@@ -778,6 +778,9 @@ app.get("/api/changeAmount/:id/:lineItemId/:quantity", async (req, res) => {
   let status = 200;
   let error = null;
   const quantity = parseInt(req.params["quantity"]);
+
+  console.log("==========staart of it")
+  console.log(orderId, lineItemId, quantity)
   try {
     const openOrder = await client.query({
       data: {
@@ -798,13 +801,13 @@ app.get("/api/changeAmount/:id/:lineItemId/:quantity", async (req, res) => {
       },
     });
 
-    //console.log('this is the mutation response 1' , openOrder.body.data.orderEditBegin)//.calculatedOrder
+    console.log('this is the mutation response 1' , openOrder.body.data.orderEditBegin)//.calculatedOrder
     const calculatedOrderId =
       openOrder.body.data.orderEditBegin.calculatedOrder.id;
-    //console.log('order id', calculatedOrderId)
+    console.log('order id', calculatedOrderId)
     //console.log("this is the line item if", lineItemId)
     const calculatedLineItem = "gid://shopify/CalculatedLineItem/" + lineItemId;
-
+    console.log("=====calc line item",calculatedLineItem)
     const changeAmount = await client.query({
       data: {
         query: `mutation orderEditSetQuantity($id: ID!, $lineItemId: ID!, $quantity: Int!) {
@@ -838,7 +841,7 @@ app.get("/api/changeAmount/:id/:lineItemId/:quantity", async (req, res) => {
       },
     });
 
-    // console.log('this is the mutation response 2' , changeAmount.body.data.orderEditSetQuantity)
+    console.log('this is the mutation response 2' , changeAmount.body.data.orderEditSetQuantity)
     const commitChange = await client.query({
       data: {
         query: `mutation orderEditCommit($id: ID!) {
@@ -859,7 +862,7 @@ app.get("/api/changeAmount/:id/:lineItemId/:quantity", async (req, res) => {
         },
       },
     });
-    //console.log('this is the mutation response' , commitChange.body.data.orderEditCommit)
+    console.log('this is the mutation response' , commitChange.body.data.orderEditCommit)
   } catch (e) {
     console.log(`Failed to edit quantity:  ${e.message}`);
     status = 500;
